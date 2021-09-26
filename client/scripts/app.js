2299a6f733c19a14a8a75d0d1ca14f5d4d18369a
Josh H <josh.terance@gmail.com>
2021-09-25 21:17:20 -0400

    backend cleanup

---
+++ client/scripts/app.js
@@ -0,0 +1,8 @@
+'use strict';
+(() => {
+    function Start() {
+        console.log("App Front End Started!");
+    }
+    window.addEventListener("load", Start);
+})();
+//# sourceMappingURL=app.js.map
---
+++ client/scripts/app.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// IIFE
+(() => {
+
+    function Start() {
+        console.log("App Front End Started!");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
