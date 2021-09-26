0b79f7e15a87644bff89498238fad34d950062db
Josh H <josh.terance@gmail.com>
2021-09-25 22:47:04 -0400

    Update copyAssets.ts
    ok im actually done today i swear

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -1,9 +1,9 @@
 /* This is a helper function that copys our resources into the dist folder before building,
-so that all the transpiled js files can access their needed resources after building. */
+so that all the transpiled js files can access their needed resources after building.
+It is used in package.json */
 
-import * as shell from "shelljs";
-// Copy all the view templates and assets in the public folder
+import shell from "shelljs";
+// Copy all the view templates and assets located in src/views and src/public
 shell.cp("-R", ["src/views", "src/public"], "dist/");
-
-// Remove unnecessary ts and json files
+// Clean up unnecessary ts and json files 
 shell.rm(["dist/public/js/*.ts", "dist/public/js/*.json"]);
